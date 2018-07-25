b1388d333567491954951443b7d646152ed5e5cc
Chris Pollard <cspollard@gmail.com>
2018-07-25 10:42:57 +0200

    print csv

--- app/Main.hs
+++ app/Main.hs
@@ -1,23 +1,29 @@
 {-# LANGUAGE DeriveGeneric       #-}
 {-# LANGUAGE OverloadedStrings   #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TupleSections       #-}
 
 module Main where
 
 import           Atlas
 import           Atlas.CrossSections
-import           Control.Applicative  (ZipList (..))
-import qualified Control.Foldl        as F
-import           Control.Lens         (view)
-import           Control.Monad        (forM_)
+import           Control.Applicative        (ZipList (..))
+import           Control.Comonad            (duplicate, extract)
+import qualified Control.Foldl              as F
+import           Control.Lens               (view)
+import           Control.Monad              (forM_)
+import           Control.Monad.Writer.Class
 import           Data.Functor.Compose
+import           Data.Maybe                 (isJust)
+-- import           Data.Histogram.Bin.BinF
+import           Data.Monoid                ((<>))
 import           Data.TFile
 import           Data.TTree
 import           GHC.Float
 import           Options.Generic
 import           Pipes
 import           Pipes.Lift
-import qualified Pipes.Prelude        as P
+import qualified Pipes.Prelude              as P
 
 data Args =
   Args
@@ -91,13 +97,20 @@ readJets = do
     cToB _ = True
 
 
-readEvent :: (MonadIO m, MonadThrow m) => TreeRead m (Int, [Jet], [RCJet])
+readEvent :: (MonadIO m, MonadThrow m) => TreeRead m (Maybe Double)
 readEvent = do
+  -- wgt <- float2Double <$> readBranch "weight_mc"
   js <- readJets
   rcjs <- readRCJets
   let nbjs = foldr (\(Jet _ tagged) s -> if tagged then s+1 else s) 0 js
+      js' = removeOverlap rcjs js
+      rcp4s = take 2 . filter ((> 80000) . view lvM) $ rcFourMom <$> rcjs
 
-  return (nbjs, removeOverlap rcjs js, rcjs)
+  if nbjs < 3 || length js' < 4 || length rcjs < 2
+    then return Nothing
+    else
+      let (rc1:rc2:_) = rcp4s
+      in return . Just . view lvM $ rc1 <> rc2
 
   where
     removeOverlap rcjs js =
@@ -110,18 +123,23 @@ main = do
   args <- getRecord "run-4t" :: IO Args
   xsec <- readXSecFile (xsecfile args)
 
-  -- get the list of input trees
-  fns <- filter (not . null) . lines <$> readFile (infiles args)
 
-  forM_ fns
-    $ \fn -> do
+  runEffect
+    $ for (linesP $ infiles args) readTree >-> P.print
+
+
+  where
+    linesP fn = do
+      s <- liftIO $ readFile fn
+      each (lines s) >-> P.filter (not . null)
+
+    readTree fn = do
       f <- tfileOpen fn
       t <- ttree f "nominal_Loose"
-      runEffect . evalStateP t
+      evalStateP t
         $ each [0..]
           >-> pipeTTree readEvent
-          >-> P.filter (\(nb, js, rcs) -> nb >= 3 && length js >= 4 && length rcs >= 2)
-          >-> P.map (\(_, _, RCJet rc1:RCJet rc2:_) -> view lvM (rc1 `mappend` rc2))
-          >-> P.print
+          >-> P.concat
+          >-> P.map (1.0,)
 
       tfileClose f
