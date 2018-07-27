0e4fdff609b906e2ff34ab87156a21205be1d8d3
Chris Pollard <cpollard@cern.ch>
2018-07-27 10:47:12 +0200

    adding some helpful scripts

---
+++ ext/makecsvs.sh
@@ -0,0 +1,2 @@
+stack exec AllForTops-exe -- --isdata --outfolder data1516 --infiles data1516.infiles > data1516/log 2>&1 &
+stack exec AllForTops-exe -- --outfolder ttmc --infiles ttmc.infiles > ttmc/log 2>&1 &
