d755053de31dbdf6601a2682be0b4df21a6678fb
Chris Pollard <cpollard@cern.ch>
2018-08-07 15:36:03 +0200

    new files

--- ext/makecsvs.sh
+++ ext/makecsvs.sh
@@ -1,2 +1,7 @@
-stack exec AllForTops-exe -- --outfolder data1516 --infiles data1516.infiles --lumi 1 --xsecfile data/XSection-MC15-13TeV.data > data1516/log 2>&1 &
-stack exec AllForTops-exe -- --outfolder ttmc --infiles ttmc.infiles --lumi 36000 --xsecfile data/XSection-MC15-13TeV.data > ttmc/log 2>&1 &
+# stack exec AllForTops-exe -- --outfolder data1516 --infiles data1516.infiles --lumi 1 --xsecfile data/XSection-MC15-13TeV.data > data1516/log 2>&1 &
+
+for f in 407342 407343 407344 410470
+do
+  mkdir -p $f.out
+  stack exec AllForTops-exe -- --outfolder $f.out --infiles $f.infiles --lumi 36000 --xsecfile data/XSection-MC15-13TeV.data > $f.out/log 2>&1 &
+done
