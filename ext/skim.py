a12841990544c369168f224c18fb4965bea3c532
Chris Pollard <cpollard@cern.ch>
2018-08-07 09:58:32 +0200

    adding skimming script

---
+++ ext/skim.py
@@ -0,0 +1,32 @@
+from sys import argv
+myargv = argv[:]
+import ROOT as R
+
+outf = R.TFile.Open(myargv[1], "CREATE")
+
+if not outf:
+  print "failed to open out file."
+  exit(-1)
+
+c = R.TChain("nominal_Loose")
+cw = R.TChain("sumWeights")
+
+for fname in myargv[2:]:
+  print "adding " + fname + " to chain."
+  c.Add(fname)
+  cw.Add(fname)
+
+
+selection = \
+  "(ejets_2015_MV2c10 || ejets_2016_MV2c10 \
+   || mujets_2015_MV2c10 || mujets_2016_MV2c10) \
+   && (Sum$(ljet_pt > 250000) || Sum$(rcjet_pt > 250000))"
+
+
+t = c.CopyTree(selection)
+t.Write()
+
+tw = cw.CopyTree("")
+tw.Write()
+outf.Write()
+
