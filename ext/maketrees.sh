21c43bdd1b3eb3e74ce4fc4e309449bc0e0016cf
Chris Pollard <cpollard@cern.ch>
2018-09-06 17:57:02 +0200

    updates to scripts

--- ext/maketrees.sh
+++ ext/maketrees.sh
@@ -1,2 +1,1 @@
-for f in ttmc/*b; do python ext/plot_root.py ${f}.root < $f; done
-for f in data1516/*b; do python ext/plot_root.py ${f}.root < $f; done
+for f in *.out/*b; do python ext/plot_root.py ${f}.root < $f; done
